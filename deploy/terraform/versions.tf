6ac15fa79db8e9eba682c35b94a297a1a9215973
theanecdote <morgan@perihelion.team>
2026-06-18 21:15:03 +0900

    infra: activate GCS backend for rocky-pi terraform state

--- deploy/terraform/versions.tf
+++ deploy/terraform/versions.tf
@@ -6,12 +6,12 @@ terraform {
       version = ">= 6.0, < 7.0"
     }
   }
-  # Recommended: store state in a GCS bucket with restricted access, because
-  # google_sql_user.password lands in state. Configure before first apply:
-  # backend "gcs" {
-  #   bucket = "perihelion-485106-tfstate"
-  #   prefix = "rocky-pi/cloudrun"
-  # }
+  # State stored in a GCS bucket with restricted access, because
+  # google_sql_user.password lands in state.
+  backend "gcs" {
+    bucket = "perihelion-485106-tfstate"
+    prefix = "rocky-pi/cloudrun"
+  }
 }
 
 provider "google" {
